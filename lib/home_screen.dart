278b06ac8aec8a6cc5065bb69e43f0a51ef27ed5
sairikwith <sairikwith443@gmail.com>
2025-04-24 02:00:25 -0700

    add week-range slider with formatted date labels

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'add_habit_flow.dart';
 import 'time_entry_screen.dart';
 import 'planner_screen.dart';
+import 'package:intl/intl.dart';
 import 'manage_habits_screen.dart';
 import 'analytics_screen.dart';
 
@@ -106,85 +107,123 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  final List<DateTime> _week = List.generate(7, (i) {
-    final now = DateTime.now();
-    final monday = now.subtract(Duration(days: now.weekday - 1));
-    return monday.add(Duration(days: i));
-  });
+  int _weekOffset = 0; // weeks from current
   int _dayIdx = DateTime.now().weekday - 1;
 
   String _label(int i) => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'][i];
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        color: Colors.teal[100],
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: List.generate(
-              7,
-              (i) => GestureDetector(
-                    onTap: () => setState(() => _dayIdx = i),
-                    child: _DateColumn(
-                      dayLabel: _label(i),
-                      dayNumber: _week[i].day.toString(),
-                      isSelected: i == _dayIdx,
-                    ),
-                  )),
+    final now = DateTime.now();
+    // Calculate start of offset week (Monday)
+    final monday = DateTime(now.year, now.month, now.day)
+        .subtract(Duration(days: now.weekday - 1))
+        .add(Duration(days: _weekOffset * 7));
+    final weekDates = List.generate(7, (i) => monday.add(Duration(days: i)));
+
+    return Column(
+      children: [
+        // Week slider
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Prev', style: TextStyle(color: Colors.grey[700])),
+                Text(
+                  // Display actual week range
+                  '${DateFormat('MMM d').format(weekDates.first)} - '
+                  '${DateFormat('MMM d').format(weekDates.last)}',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text('Next', style: TextStyle(color: Colors.grey[700])),
+              ],
+            ),
+            Slider(
+              value: _weekOffset.toDouble(),
+              min: -4,
+              max: 4,
+              divisions: 8,
+              label:
+                  '${DateFormat('MMM d').format(weekDates.first)} - '
+                  '${DateFormat('MMM d').format(weekDates.last)}',
+              activeColor: Colors.teal,
+              inactiveColor: Colors.teal.shade100,
+              onChanged: (val) {
+                setState(() {
+                  _weekOffset = val.toInt();
+                  _dayIdx = 0;
+                });
+              },
+            ),
+          ]),
         ),
-      ),
-      Expanded(
-        child: StreamBuilder<List<Habit>>(
-          stream: HabitService().habitsForDay(_dayIdx),
-          builder: (_, snap) {
-            if (snap.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            final habits = snap.data ?? [];
-            // **SORT ASCENDING BY targetTime**
-            habits.sort((a, b) {
-              final aMin = a.targetTime.hour * 60 + a.targetTime.minute;
-              final bMin = b.targetTime.hour * 60 + b.targetTime.minute;
-              return aMin.compareTo(bMin);
-            });
-
-            final morning =
-                habits.where((h) => h.targetTime.hour < 12).toList();
-            final noon = habits
-                .where((h) => h.targetTime.hour >= 12 && h.targetTime.hour < 17)
-                .toList();
-            final evening =
-                habits.where((h) => h.targetTime.hour >= 17).toList();
-
-            return SingleChildScrollView(
-  child: Column(children: [
-    _TimeBlockCard(
-      blockTitle: 'Morning',
-      habits: morning,
-      date: _week[_dayIdx],
-      onSessionLogged: () => setState(() {}),
-    ),
-    _TimeBlockCard(
-      blockTitle: 'Noon',
-      habits: noon,
-      date: _week[_dayIdx],
-      onSessionLogged: () => setState(() {}),
-    ),
-    _TimeBlockCard(
-      blockTitle: 'Evening',
-      habits: evening,
-      date: _week[_dayIdx],
-      onSessionLogged: () => setState(() {}),
-    ),
-  ]),
-);
-
-          },
+        // Day labels row
+        Container(
+          color: Colors.teal[100],
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: List.generate(7, (i) {
+              return GestureDetector(
+                onTap: () => setState(() => _dayIdx = i),
+                child: _DateColumn(
+                  dayLabel: _label(i),
+                  dayNumber: weekDates[i].day.toString(),
+                  isSelected: i == _dayIdx,
+                ),
+              );
+            }),
+          ),
         ),
-      ),
-    ]);
+        // Habit list
+        Expanded(
+          child: StreamBuilder<List<Habit>>(
+            stream: HabitService().habitsForDay(_dayIdx),
+            builder: (_, snap) {
+              if (snap.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final habits = snap.data ?? [];
+              habits.sort((a, b) {
+                final aMin = a.targetTime.hour * 60 + a.targetTime.minute;
+                final bMin = b.targetTime.hour * 60 + b.targetTime.minute;
+                return aMin.compareTo(bMin);
+              });
+              final morning = habits.where((h) => h.targetTime.hour < 12).toList();
+              final noon = habits
+                  .where((h) => h.targetTime.hour >= 12 && h.targetTime.hour < 17)
+                  .toList();
+              final evening = habits.where((h) => h.targetTime.hour >= 17).toList();
+
+              return SingleChildScrollView(
+                child: Column(children: [
+                  _TimeBlockCard(
+                    blockTitle: 'Morning',
+                    habits: morning,
+                    date: weekDates[_dayIdx],
+                    onSessionLogged: () => setState(() {}),
+                  ),
+                  _TimeBlockCard(
+                    blockTitle: 'Noon',
+                    habits: noon,
+                    date: weekDates[_dayIdx],
+                    onSessionLogged: () => setState(() {}),
+                  ),
+                  _TimeBlockCard(
+                    blockTitle: 'Evening',
+                    habits: evening,
+                    date: weekDates[_dayIdx],
+                    onSessionLogged: () => setState(() {}),
+                  ),
+                ]),
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
 
